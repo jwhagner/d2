7964594da1828d13a93460b161b8aa4fff98a8cf
Joey <jwh33@pitt.edu>
2015-02-02 17:02:15 -0500

    Added initial files

---
+++ src/tests/TestRun.java
@@ -0,0 +1,19 @@
+package tests;
+
+import com.laboon.*;
+
+import static org.junit.Assert.*;
+import org.mockito.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestRun {
+
+	@Test
+	public void test() {
+		assertEquals(4,4);
+	}
+
+}
