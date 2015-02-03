7dcc49e7269f6bb2e929403d752a54015ce53336
Joey <jwh33@pitt.edu>
2015-02-02 20:55:06 -0500

    Added some tests

--- src/tests/TestRun.java
+++ src/tests/TestRun.java
@@ -3,17 +3,69 @@ package tests;
 import com.laboon.*;
 
 import static org.junit.Assert.*;
-import org.mockito.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestRun {
+	
+	
+	@Test
+	public void testGetSugar() {
+		fail("Not implemented");
+	}
+	
+	// Tests Game.doSomething() with the input 'N'
+	@Test
+	public void testDoSomethingNorth() {
+		House mockHouse = mock(House.class);
+		Game g = new Game(new Player(), mockHouse);
+		int retValue = g.doSomething("N");
+		verify(mockHouse, times(1)).moveNorth();
+		
+		assertEquals(retValue, 0);
+	}
+	
+	// Tests Game.doSomething() with the input 'S'
+	@Test
+	public void testDoSomethingSouth() {
+		House mockHouse = mock(House.class);
+		Game g = new Game(new Player(), mockHouse);
+		int retValue = g.doSomething("S");
+		verify(mockHouse, times(1)).moveSouth();
+		
+		assertEquals(retValue, 0);
+	}
+	
+	@Test
+	public void testDoSomethingLook() {
+		fail("Not implemented");
+	}
 
 	@Test
-	public void test() {
-		assertEquals(4,4);
+	public void testDoSomethingInventory() {
+		fail("Not implemented");
+	}
+	
+	@Test
+	public void testDoSomethingDrink() {
+		fail("Not implemented");
+	}
+	
+	@Test
+	public void testDoSomethingHelp() {
+		fail("Not implemented");
+	}
+	
+	// Tests House.getCurrentRoomInfo()
+	@Test
+	public void testGetCurrentRoomInfo() {
+		House h = new House(5);
+		String message = h.getCurrentRoomInfo();
+		
+		Assert
 	}
 
 }
